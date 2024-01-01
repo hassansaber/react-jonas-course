df6afe43ccd44c66dbcf49dd28e389414a54942d
hassansaber <hassansaberkarin@gmail.com>
2024-01-01 11:59:18 +0330

    .

---
+++ 11-worldwise/src/App.jsx
@@ -0,0 +1,71 @@
+import { Routes, BrowserRouter, Route } from "react-router-dom";
+
+import HomePage from "./pages/Homepage";
+import Pricing from "./pages/Pricing";
+import Product from "./pages/Product";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
+import { useEffect, useState } from "react";
+import CityList from "./components/CityList";
+import CountryList from "./components/CountryList";
+
+const BASE_URL = "http://localhost:8000/";
+
+function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [cities, setCities] = useState([]);
+
+  useEffect(function () {
+    async function getCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch (err) {
+        throw new Error(err.message);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    getCities();
+  }, []);
+
+  // console.log(countries);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path="product" element={<Product />} />
+        <Route path="pricing" element={<Pricing />} />
+        <Route path="login" element={<Login />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route
+            index
+            element={
+              <CityList cities={cities} isLoading={isLoading} />
+            }
+          />
+          <Route
+            path="cities"
+            element={
+              <CityList cities={cities} isLoading={isLoading} />
+            }
+          />
+          <Route
+            path="countries"
+            element={
+              <CountryList cities={cities} isLoading={isLoading} />
+            }
+          />
+          <Route path="form" element={<p>form</p>} />
+        </Route>
+
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ 11-worldwise/src/components/FooterApp.jsx
@@ -0,0 +1,13 @@
+import styles from "./FooterApp.module.css";
+
+function FooterApp() {
+  return (
+    <footer className={styles.footer}>
+      <p className={styles.copyright}>
+        &copy; Copyright {new Date().getFullYear()} by WorldWise Inc.
+      </p>
+    </footer>
+  );
+}
+
+export default FooterApp;
