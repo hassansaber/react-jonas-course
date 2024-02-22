b6974f113ff16a65a8743064eac112086cb9bdb7
hassansaber <hassansaberkarin@gmail.com>
2024-02-22 20:43:17 +0330

    Fetching city data in the form

--- 11-worldwise/src/components/Form.jsx
+++ 11-worldwise/src/components/Form.jsx
@@ -8,6 +8,7 @@ import Button from "./Button";
 import styles from "./Form.module.css";
 import BackButton from "./BackButton";
 import Spinner from "./Spinner";
+import Message from "./Message";
 
 export function convertToEmoji(countryCode) {
   const codePoints = countryCode
@@ -17,41 +18,53 @@ export function convertToEmoji(countryCode) {
   return String.fromCodePoint(...codePoints);
 }
 
+const BASE_URL =
+  "https://api.bigdatacloud.net/data/reverse-geocode-client";
+
 function Form() {
+  const [lat, lng] = useUrlPosition();
+
   const [isLoadingGeocoding, setIsLoadingGeocoding] = useState(false);
   const [cityName, setCityName] = useState("");
   const [country, setCountry] = useState("");
   const [date, setDate] = useState(new Date());
   const [notes, setNotes] = useState("");
-  const [lat, lng] = useUrlPosition();
   const [emoji, setEmoji] = useState("");
-
-  const BASE_URL =
-    "https://api.bigdatacloud.net/data/reverse-geocode-client";
+  const [geocodingError, setGeocodingError] = useState("");
 
   useEffect(
     function () {
-      try {
-        setIsLoadingGeocoding(true);
-        async function fetchCityData() {
+      async function fetchCityData() {
+        try {
+          setGeocodingError("");
+          setIsLoadingGeocoding(true);
           const res = await fetch(
             `${BASE_URL}?latitude=${lat}&longitude=${lng}`
           );
           const data = await res.json();
-          console.log(data);
+
+          if (!data.countryCode)
+            throw new Error(
+              "👋 That doesn't seem to be a city. Click somewhere else. 😉"
+            );
+
           setCityName(data.city || data.locality || "");
-          setCountry(data.countryCode || "");
+          setCountry(data.countryName);
+          setEmoji(convertToEmoji(data.countryCode));
+        } catch (err) {
+          setGeocodingError(err.message);
+        } finally {
+          setIsLoadingGeocoding(false);
         }
-        fetchCityData();
-      } catch (err) {
-      } finally {
-        setIsLoadingGeocoding(false);
       }
+      fetchCityData();
     },
     [lat, lng]
   );
 
   if (isLoadingGeocoding) return <Spinner />;
+
+  if (geocodingError) return <Message message={geocodingError} />;
 
   return (
     <form className={styles.form}>
