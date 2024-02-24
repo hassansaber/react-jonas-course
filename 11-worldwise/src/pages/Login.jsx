3cc0a13b923c33aa44ea856dba6209d18122930c
hassansaber <hassansaberkarin@gmail.com>
2024-02-24 20:54:57 +0330

    Adding Fake Authentication Implementing login

--- 11-worldwise/src/pages/Login.jsx
+++ 11-worldwise/src/pages/Login.jsx
@@ -1,11 +1,29 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import PageNav from "../components/PageNav";
 import styles from "./Login.module.css";
+import Button from "../components/Button";
+
+import { useAuth } from "../contexts/FakeAuthContext";
+import { useNavigate } from "react-router-dom";
 
 export default function Login() {
   // PRE-FILL FOR DEV PURPOSES
   const [email, setEmail] = useState("jack@example.com");
   const [password, setPassword] = useState("qwerty");
+  const { login, isAuthenticated } = useAuth();
+  const navigate = useNavigate();
+
+  useEffect(
+    function () {
+      if (isAuthenticated) navigate("/app/");
+    },
+    [isAuthenticated]
+  );
+
+  function handleClick(e) {
+    e.preventDefault();
+    login(email, password);
+  }
 
   return (
     <main className={styles.login}>
@@ -32,7 +50,9 @@ export default function Login() {
         </div>
 
         <div>
-          <button>Login</button>
+          <Button type="primary" onClick={handleClick}>
+            Login
+          </Button>
         </div>
       </form>
     </main>
