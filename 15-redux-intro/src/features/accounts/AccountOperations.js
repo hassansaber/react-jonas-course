836891e159066fbb658e1c8fea2f8b73708709eb
Hassan Saber <hassansaberkarin@gmail.com>
2024-03-05 15:07:54 +0330

    275 - create account slice with RTK

--- 15-redux-intro/src/features/accounts/AccountOperations.js
+++ 15-redux-intro/src/features/accounts/AccountOperations.js
@@ -13,7 +13,8 @@ function AccountOperations() {
 
   function handleDeposit() {
     if (!depositAmount) return;
-    dispatch(deposit(depositAmount, currency))
+    // dispatch(deposit(depositAmount, currency))
+    dispatch(deposit(depositAmount))
     setDepositAmount('')
     setCurrency('USD')
   }
