914ded04af2b441b3477459f5f7d2b1dff035007
Hassan Saber <hassansaberkarin@gmail.com>
2024-03-07 02:29:45 +0330

    289 - Handling Errors With Error Elements

--- 16-fast-react-pizza/src/App.jsx
+++ 16-fast-react-pizza/src/App.jsx
@@ -9,17 +9,24 @@ import Order from "./features/order/Order";
 import CreateOrder from "./features/order/CreateOrder";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import AppLayout from "./ui/AppLayout";
+import Error from "./ui/Error";
 
 function App() {
   const router = createBrowserRouter([
     {
       element: <AppLayout />, // Layout rout : without path
+      errorElement: <Error />,
       children: [
         { path: "/", element: <Home /> },
         { path: "cart", element: <Cart /> },
         { path: "order/:orderId", element: <Order /> },
         { path: "order/new", element: <CreateOrder /> },
-        { path: "menu", element: <Menu />, loader: menuLoader },
+        {
+          path: "menu",
+          element: <Menu />,
+          loader: menuLoader,
+          errorElement: <Error />,
+        },
       ],
     },
   ]);
--- 16-fast-react-pizza/src/UI/Error.jsx
+++ 16-fast-react-pizza/src/UI/Error.jsx
@@ -1,15 +1,33 @@
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useRouteError } from "react-router-dom";
 
-function NotFound() {
+function Error() {
   const navigate = useNavigate();
+  const error = useRouteError();
 
   return (
     <div>
       <h1>Something went wrong 😢</h1>
-      <p>%MESSAGE%</p>
+      <p>{error.data || error.message}</p>
       <button onClick={() => navigate(-1)}>&larr; Go back</button>
     </div>
   );
 }
 
-export default NotFound;
+export default Error;
+
+/*
+Error Handling :
+in RRD v6
+error bubble up into parent route
+when each parent or even the errorRoute has
+errorElement on his routObject ,
+Error component will replace with that route Element
+
+
+
+Error handling steps :
+1) create Error component   
+2) provide error  - errorElement in routeObject 
+3) provide error to the page - useRouteError hook in Error component
+3.5) error.data for data errors , error.message for fetch errors
+*/
