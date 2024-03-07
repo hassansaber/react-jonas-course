ca367b9bcf7b163b8b44e9a64fb282b6be75114e
Hassan Saber <hassansaberkarin@gmail.com>
2024-03-07 14:15:01 +0330

    295 - Setting Up Tailwind CSS

--- 16-fast-react-pizza/src/UI/Home.jsx
+++ 16-fast-react-pizza/src/UI/Home.jsx
@@ -1,7 +1,7 @@
 function Home() {
   return (
     <div>
-      <h1>
+      <h1 className=" text-center text-xl font-semibold text-yellow-500">
         The best pizza.
         <br />
         Straight out of the oven, straight to you.
@@ -11,3 +11,16 @@ function Home() {
 }
 
 export default Home;
+
+/*
+Tailwind Setup 
+1) tailwind config on Doc
+2) tailwind extension 
+3) npm install -D prettier prettier-plugin-tailwindcss
+4) config 3  based on Doc 
+ prettier.config.js  =>>> prettier.config.cjs
+  module.exports = {
+    tailwindConfig: './styles/tailwind.config.js',
+  }
+
+*/
