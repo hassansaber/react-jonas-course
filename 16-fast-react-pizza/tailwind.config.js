028e82e9f318eda511a30d65fe6bbf22aee091ba
Hassan Saber <hassansaberkarin@gmail.com>
2024-03-07 16:38:54 +0330

    297 - Styling Text

--- 16-fast-react-pizza/tailwind.config.js
+++ 16-fast-react-pizza/tailwind.config.js
@@ -10,3 +10,19 @@ export default {
   plugins: [],
 }
 
+
+/*
+Tailwind : 
+  *arbitrary value  :  [2rem] ex: text-[2rem]*
+
+  text color : text-yellow-500 
+  background color : bg-stone-800
+  font size ,
+  line height : text-sm
+  letter spacing : tracking-widest
+  text transform uppercase : uppercase
+  
+
+
+
+*/
