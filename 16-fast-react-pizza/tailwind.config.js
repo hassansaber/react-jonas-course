802936eb8cfa370eae0020059289b701d22da7c0
hassansaber <hassansaberkarin@gmail.com>
2024-03-07 21:20:14 +0330

    299 - Responsive Design

--- 16-fast-react-pizza/tailwind.config.js
+++ 16-fast-react-pizza/tailwind.config.js
@@ -30,9 +30,24 @@ Tailwind :
     margin : m-10 , mx-4 , mt-2 , mr-auto 
     padding : p-8
     border: border-b-4 border-stone-400
-    spacing between children : space-x-4 (no need flex trickes)
+    spacing between children : space-x-4 (no need flex tricks)
     display none : hidden , flex , grid ,block...
  
+  Responsive-Design:
+    *mobile-first* : all classes without prefix ,are based on smallest screen
+    @media (min-width: 640px) { ... } : sm    ex: sm:text-base
+    Prefixes :
+      sm: from 640px
+      md: from 768px
+      lg: from 1024px
+      xl: from 1280px
+      2xl: from 1536px
+    Best approuch : first build for mobile devices (
+      all screens below 640px  width
+    )
+
+
+    
 
 
 
