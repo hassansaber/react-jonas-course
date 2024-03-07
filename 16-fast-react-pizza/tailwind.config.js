252414257a70eb1ad3a1ce0ea5a9ddaa5701b340
hassansaber <hassansaberkarin@gmail.com>
2024-03-07 18:52:28 +0330

    298 - The Box Model Spacing Borders and Display

--- 16-fast-react-pizza/tailwind.config.js
+++ 16-fast-react-pizza/tailwind.config.js
@@ -16,13 +16,24 @@ Tailwind :
   *arbitrary value  :  [2rem]  
     font size ex : text-[2rem]*
 
+  Color:
+    text color : text-yellow-500 
+    background color : bg-stone-800
 
-  text color : text-yellow-500 
-  background color : bg-stone-800
-  font size ,
-  line height : text-sm
-  letter spacing : tracking-widest
-  text transform uppercase : uppercase
+  Text-Style:
+    font size ,
+    line height : text-sm
+    letter spacing : tracking-widest
+    text transform uppercase : uppercase
+
+  Box-Model,Spacing:
+    margin : m-10 , mx-4 , mt-2 , mr-auto 
+    padding : p-8
+    border: border-b-4 border-stone-400
+    spacing between children : space-x-4 (no need flex trickes)
+    display none : hidden , flex , grid ,block...
+ 
+
 
 
 
