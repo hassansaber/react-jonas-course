6104e4483c4da1283ac83e10a8910b5d31b6d3cf
Hassan Saber <hassansaberkarin@gmail.com>
2024-03-07 03:46:45 +0330

    290 - Fetching Orders - useParam alternative in RRD v6

--- 16-fast-react-pizza/src/App.jsx
+++ 16-fast-react-pizza/src/App.jsx
@@ -3,24 +3,29 @@ import {
   createBrowserRouter,
 } from "react-router-dom";
 
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
+import Order, { loader as orderLoader } from "./features/order/Order";
 import Home from "./ui/Home";
 import Cart from "./features/cart/Cart";
-import Order from "./features/order/Order";
 import CreateOrder from "./features/order/CreateOrder";
-import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import AppLayout from "./ui/AppLayout";
 import Error from "./ui/Error";
 
 function App() {
   const router = createBrowserRouter([
     {
-      element: <AppLayout />, // Layout rout : without path
+      element: <AppLayout />, // Layout route : routeObj  without path
       errorElement: <Error />,
       children: [
         { path: "/", element: <Home /> },
         { path: "cart", element: <Cart /> },
-        { path: "order/:orderId", element: <Order /> },
         { path: "order/new", element: <CreateOrder /> },
+        {
+          path: "order/:orderId",
+          element: <Order />,
+          loader: orderLoader,
+          errorElement: <Error />,
+        },
         {
           path: "menu",
           element: <Menu />,
