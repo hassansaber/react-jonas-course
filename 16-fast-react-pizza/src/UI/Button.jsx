cefc78184bf61855b5c2e564f73509286404fbd6
hassansaber <hassansaberkarin@gmail.com>
2024-03-20 19:55:23 +0330

    310 - Styling the Order Form

--- 16-fast-react-pizza/src/UI/Button.jsx
+++ 16-fast-react-pizza/src/UI/Button.jsx
@@ -11,7 +11,7 @@ function Button({ children, disabled, to, type }) {
     focus:ring-yellow-300 focus:ring-offset-2 
  disabled:cursor-not-allowed sm:px-6 sm:py-4`;
 
-  const base = `inline-block
+  const base = `inline-block text-sm
     rounded-full bg-yellow-400  
     font-semibold uppercase
     tracking-wide text-stone-800 
@@ -24,7 +24,7 @@ function Button({ children, disabled, to, type }) {
   const styles = {
     primary: base + ' px-4 py-3 md:px-6 md:py-4',
     small: base + ' px-4 py-2 md:px-5 md:py-2.5 text-xs ',
-    secondary: `inline-block
+    secondary: `inline-block text-sm
       rounded-full border-2 border-stone-300  
       font-semibold uppercase
       tracking-wide text-stone-400 
