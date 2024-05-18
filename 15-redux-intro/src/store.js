21c6c54232320b13ab7777a30f22b08289ed7457
hassansaber <hassansaberkarin@gmail.com>
2024-05-18 21:52:37 +0330

    .

--- 15-redux-intro/src/store.js
+++ 15-redux-intro/src/store.js
@@ -1,4 +1,4 @@
-import { configureStore } from "@reduxjs/toolkit"
+import { configureStore } from '@reduxjs/toolkit'
 import accountReducer from "./features/accounts/accountSlice"
 import customerReducer from "./features/customers/customerSlice"
 
