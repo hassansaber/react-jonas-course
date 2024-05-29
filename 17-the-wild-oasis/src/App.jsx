bb993c58f4893f4c7a8859eed03b46bb261a2ac6
Hassan Saber <hassansaberkarin@gmail.com>
2024-05-30 01:46:25 +0330

    329 - Introduction to Styled Components

--- 17-the-wild-oasis/src/App.jsx
+++ 17-the-wild-oasis/src/App.jsx
@@ -1,5 +1,32 @@
 function App() {
-  return <div>Hello world</div>;
+  return <div>Hi</div>;
 }
 
 export default App;
+
+// STYLED COMPONENTS
+
+// // step-1
+// import styled from "styled-components";
+
+// // step-2 : way1
+// // generate a NamedStyledComponent from html tag
+// const Input = styled.h1`
+//   font-size: 5rem;
+// `;
+
+// // step-2 : way2
+// // change existed component into styled
+// const StyledApp = styled.div`
+//   background-color: orange;
+// `;
+
+// function App() {
+//   return (
+//     // usage : really simple
+//     <StyledApp>
+//       <Input>Hello world</Input>
+//     </StyledApp>
+//   );
+// }
+// export default App;
