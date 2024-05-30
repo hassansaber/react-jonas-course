4b27924e71276c749e6e63e8a0349b0bbe773e87
hassansaber <hassansaberkarin@gmail.com>
2024-05-30 19:53:46 +0330

    330 - Global Styles With Styled Components

--- 17-the-wild-oasis/src/App.jsx
+++ 17-the-wild-oasis/src/App.jsx
@@ -1,29 +1,12 @@
 import styled from "styled-components";
 import GlobalStyles from "./styles/GlobalStyle";
+import Input from "./ui/Input";
+import Button from "./ui/Button";
 
 const H1 = styled.h1`
   font-size: 30px;
   font-weight: 600;
   background-color: yellow;
-`;
-
-const Button = styled.button`
-  font-size: 1.4rem;
-  padding: 1.2rem 1.6rem;
-  font-weight: 500;
-  border: none;
-  border-radius: var(--border-radius-sm);
-  background-color: var(--color-brand-600);
-  color: var(--color-brand-50);
-  box-shadow: var(--shadow-sm);
-  cursor: pointer;
-`;
-
-const Input = styled.input`
-  border: 1px solid var(--color-grey-300);
-  background-color: var(--color-grey-0);
-  border-radius: var(--border-radius-sm);
-  padding: 0.8rem 1.2rem;
 `;
 
 const StyledApp = styled.main`
