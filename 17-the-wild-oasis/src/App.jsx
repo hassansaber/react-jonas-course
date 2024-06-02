59586b100937ce4cef6c5f7d9420c599f42744b7
Hassan Saber <hassansaberkarin@gmail.com>
2024-06-03 02:51:51 +0330

    333 - Setting Up Pages and Routes

--- 17-the-wild-oasis/src/App.jsx
+++ 17-the-wild-oasis/src/App.jsx
@@ -1,82 +1,41 @@
-import styled from "styled-components";
-import GlobalStyles from "./styles/GlobalStyle";
-import Input from "./ui/Input";
-import Button from "./ui/Button";
-import Heading from "./ui/Heading";
-import Row from "./ui/Row";
-
-const StyledApp = styled.main`
-  background-color: orangered;
-  padding: 20px;
-`;
+import {
+  BrowserRouter,
+  Navigate,
+  Route,
+  Routes,
+} from "react-router-dom";
+import GlobalStyle from "./styles/GlobalStyle";
+import Dashboard from "./pages/Dashboard";
+import Account from "./pages/Account";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import Login from "./pages/Login";
+import Settings from "./pages/Settings";
+import Users from "./pages/Users";
+import PageNotFound from "./pages/PageNotFound";
 
 function App() {
   return (
     <>
-      <GlobalStyles />
-      <StyledApp>
-        <Heading as="h1">The Wild Oasis</Heading>
-        <Row>
-          <Row type="horizontal">
-            <Heading as="h2">Check in and out</Heading>
-
-            <div>
-              <Button onClick={() => alert("check in")}>
-                Check in
-              </Button>
-              <Button
-                variation="secondary"
-                size="small"
-                onClick={() => alert("check out")}
-              >
-                Check out
-              </Button>
-            </div>
-          </Row>
-
-          <Row type="vertical">
-            <Heading as="h3">Forms</Heading>
-
-            <form>
-              <Input type="number" placeholder="Number of guests" />
-              <Input type="number" placeholder="Number of guests" />
-            </form>
-          </Row>
-        </Row>
-      </StyledApp>
+      <GlobalStyle />
+      <BrowserRouter>
+        <Routes>
+          <Route
+            index
+            element={<Navigate replace to="dashboard" />}
+          />
+          <Route path="dashboard" element={<Dashboard />} />
+          <Route path="account" element={<Account />} />
+          <Route path="bookings" element={<Bookings />} />
+          <Route path="cabins" element={<Cabins />} />
+          <Route path="login" element={<Login />} />
+          <Route path="settings" element={<Settings />} />
+          <Route path="users" element={<Users />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
 
 export default App;
-
-// STYLED COMPONENTS
-
-// // step-1
-// import styled from "styled-components";
-
-// // step-2 : way1
-// // generate a NamedStyledComponent from html tag
-// const Input = styled.h1`
-//   font-size: 5rem;
-// `;
-
-// // step-2 : way2
-// // change existed component into styled
-// const StyledApp = styled.div`
-//   background-color: orange;
-// `;
-
-// function App() {
-//   return (
-//     // usage : really simple
-//     <StyledApp>
-//       <Input>Hello world</Input>
-//     </StyledApp>
-//   );
-// }
-// export default App;
-
-// styles/Global Style
-
-// ui/heading => conditional component style
