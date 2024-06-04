5012e2745691774c584ed83d8b8f2082b2de0173
hassansaber <hassansaberkarin@gmail.com>
2024-06-05 00:11:28 +0330

    353 - Handling Form Errors

--- 17-the-wild-oasis/src/ui/AppLayout.jsx
+++ 17-the-wild-oasis/src/ui/AppLayout.jsx
@@ -7,6 +7,8 @@ import Sidebar from "./Sidebar";
 const Main = styled.main`
   background-color: var(--color-grey-50);
   padding: 4rem 4.8rem 6.4rem;
+
+  overflow: scroll; // fix(freeze) sidebar while main scrolls
 `;
 
 const StyledAppLayout = styled.div`
