216afaeb986936905a50889884a4bded34b383d1
hassansaber <hassansaberkarin@gmail.com>
2024-06-05 15:41:50 +0330

    354 - Uploading Images to Supabase

--- 17-the-wild-oasis/src/features/cabins/CreateCabinForm.jsx
+++ 17-the-wild-oasis/src/features/cabins/CreateCabinForm.jsx
@@ -33,7 +33,7 @@ function CreateCabinForm() {
   });
 
   function onSubmit(data) {
-    mutate(data);
+    mutate({ ...data, image: data.image[0] });
   }
 
   function onError(errors) {
@@ -114,12 +114,14 @@ function CreateCabinForm() {
         />
       </FormRow>
 
-      <FormRow>
-        {/* <Label htmlFor="image">Cabin photo</Label> */}
+      <FormRow label="Cabin photo">
         <FileInput
           id="image"
           accept="image/*"
           disabled={isCreating}
+          {...register("image", {
+            required: "This field is required",
+          })}
         />
       </FormRow>
 
--- 17-the-wild-oasis/src/services/apiCabins.js
+++ 17-the-wild-oasis/src/services/apiCabins.js
@@ -1,4 +1,4 @@
-import supabase from './supabase'
+import supabase, { supabaseUrl } from './supabase'
 
 
 export async function getCabins() {
@@ -32,15 +32,36 @@ export async function deleteCabin(id) {
 
 export async function createCabin(newCabin) {
 
+  const imageName = `${Math.random()}-${newCabin.image.name}`.replaceAll('/', '')
+  const imagePath = `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`
+
+
+  // 1.create cabin
   const { data, error } = await supabase
     .from('cabins')
-    .insert([newCabin])
+    .insert([{ ...newCabin, image: imagePath }])
     .select()
 
   if (error) {
     console.error(error)
     throw new Error('Cabin could not be created')
   }
+
+
+  // 2.upload image
+  const { error: uploadError } = await supabase
+    .storage
+    .from('cabin-images')
+    .upload(imageName, newCabin.image)  //insert the file (newCabin.image) not path (imagePath)
+
+  // 3.delete the cabin if there was an error uploading image
+  if (uploadError) {
+    console.error(uploadError)
+    throw new Error('Cabin image could not be uploaded and the cabin was not created')
+  }
+
+
+
 
   return data
 
