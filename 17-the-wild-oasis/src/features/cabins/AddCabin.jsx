bbdb9e31abec256c8addbd0f3e013289013df5f0
hassansaber <hassansaberkarin@gmail.com>
2024-06-13 20:58:59 +0330

    .

--- 17-the-wild-oasis/src/features/cabins/AddCabin.jsx
+++ 17-the-wild-oasis/src/features/cabins/AddCabin.jsx
@@ -3,21 +3,24 @@ import Button from "../../ui/Button";
 import Modal from "../../ui/Modal";
 import CreateCabinForm from "./CreateCabinForm";
 
+// compound component:
+// why ? not want component who used modal (AddCabin) be responsible for creating state (isOpenModal)
+// (modal) itself should track wether its open or not
+
 function AddCabin() {
   const [isOpenModal, setIsOpenModal] = useState(false);
 
   return (
     <>
-      {isOpenModal ? (
+      <Button onClick={() => setIsOpenModal((show) => !show)}>
+        Add new cabin
+      </Button>
+      {isOpenModal && (
         <Modal onClose={() => setIsOpenModal(false)}>
           <CreateCabinForm
             onCloseModal={() => setIsOpenModal(false)}
           />
         </Modal>
-      ) : (
-        <Button onClick={() => setIsOpenModal((show) => !show)}>
-          Add new cabin
-        </Button>
       )}
     </>
   );
