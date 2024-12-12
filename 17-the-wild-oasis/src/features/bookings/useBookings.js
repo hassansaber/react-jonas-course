1fe62f789363f526025606b9a8c9500264748df2
hassansaber <hassansaberkarin@gmail.com>
2024-12-12 15:29:53 +0330

    382 - Prefetching With React Query

--- 17-the-wild-oasis/src/features/bookings/useBookings.js
+++ 17-the-wild-oasis/src/features/bookings/useBookings.js
@@ -1,15 +1,18 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { getBookings } from "../../services/apiBookings";
 import { useSearchParams } from "react-router-dom";
+import { PAGE_SIZE } from "../../utils/constants";
 
 export function useBookings() {
   const [searchParams] = useSearchParams();
+  const queryClient = useQueryClient();
 
   // FILTER
   const filtervalue = searchParams.get("status");
-  let filter;
-  if (filtervalue !== "all")
-    filter = { field: "status", value: filtervalue, method: "eq" };
+  let filter =
+    filtervalue === "all" || !filtervalue
+      ? null
+      : { field: "status", value: filtervalue, method: "eq" };
 
   // SORT
   const sortByValue = searchParams.get("sortBy") || "startDate-desc";
@@ -21,6 +24,7 @@ export function useBookings() {
   // PAGINATION
   const page = !searchParams.get("page") ? 1 : Number(searchParams.get("page"));
 
+  // QUERY
   const {
     isLoading,
     data: { data: bookings, count } = {},
@@ -29,6 +33,23 @@ export function useBookings() {
     queryKey: ["bookings", filter, sortBy, page], //unique name for cache
     queryFn: () => getBookings({ filter, sortBy, page }), //fn should return promise
   });
+
+  // PRE-FETCHING
+  const pageCount = Math.ceil(count / PAGE_SIZE);
+
+  // next
+  if (page < pageCount)
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", filter, sortBy, page + 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page + 1 }),
+    });
+
+  // prev
+  if (page > 1)
+    queryClient.prefetchQuery({
+      queryKey: ["bookings", filter, sortBy, page - 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page - 1 }),
+    });
 
   return { isLoading, bookings, error, count };
 }
