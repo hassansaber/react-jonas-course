372e857a78f210345fa7328645bd6feab3fb6d49
hassansaber <hassansaberkarin@gmail.com>
2024-12-22 16:40:08 +0330

    solved

--- 17-the-wild-oasis/src/features/authentication/useLogin.js
+++ 17-the-wild-oasis/src/features/authentication/useLogin.js
@@ -1,18 +1,18 @@
-import { useMutation } from "@tanstack/react-query";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { login as loginApi } from "../../services/apiAuth";
 import toast from "react-hot-toast";
 import { useNavigate } from "react-router-dom";
 
 export const useLogin = () => {
   const navigate = useNavigate();
-  // const queryClient = useQueryClient();
+  const queryClient = useQueryClient();
 
   const { mutate: login, isLoading: isLoggingin } = useMutation({
     mutationFn: ({ email, password }) => loginApi({ email, password }),
 
     onSuccess: (user) => {
       navigate("/dashboard");
-      // queryClient.setQueryData(["user"], user);
+      queryClient.setQueriesData(["user"], user);
     },
 
     onError: (error) => {
