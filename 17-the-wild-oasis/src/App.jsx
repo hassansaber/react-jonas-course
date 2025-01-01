82ecb6059ee95cd891d7bb2dbb7f07cf95e695cc
hassansaber <hassansaberkarin@gmail.com>
2025-01-01 19:32:14 +0330

    397 - Implementing Dark Mode With CSS Variables

--- 17-the-wild-oasis/src/App.jsx
+++ 17-the-wild-oasis/src/App.jsx
@@ -16,6 +16,7 @@ import AppLayout from "./ui/AppLayout";
 import Booking from "./pages/Booking";
 import CheckIn from "./pages/CheckIn";
 import ProtectedRoute from "./ui/ProtectedRoute";
+import { DarkModeProvider } from "./context/DarkModeContext";
 
 function App() {
   const queryClient = new QueryClient({
@@ -28,60 +29,62 @@ function App() {
   });
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <ReactQueryDevtools initialIsOpen={false} />
-      <GlobalStyle />
-      <BrowserRouter>
-        <Routes>
-          <Route
-            element={
-              <ProtectedRoute>
-                <AppLayout />
-              </ProtectedRoute>
-            }
-          >
-            <Route index element={<Navigate replace to="dashboard" />} />
-            <Route path="dashboard" element={<Dashboard />} />
-            <Route path="account" element={<Account />} />
-            <Route path="bookings" element={<Bookings />} />
-            <Route path="bookings/:bookingId" element={<Booking />} />
-            <Route path="checkin/:bookingId" element={<CheckIn />} />
+    <DarkModeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <GlobalStyle />
+        <BrowserRouter>
+          <Routes>
+            <Route
+              element={
+                <ProtectedRoute>
+                  <AppLayout />
+                </ProtectedRoute>
+              }
+            >
+              <Route index element={<Navigate replace to="dashboard" />} />
+              <Route path="dashboard" element={<Dashboard />} />
+              <Route path="account" element={<Account />} />
+              <Route path="bookings" element={<Bookings />} />
+              <Route path="bookings/:bookingId" element={<Booking />} />
+              <Route path="checkin/:bookingId" element={<CheckIn />} />
 
-            <Route path="cabins" element={<Cabins />} />
-            <Route path="settings" element={<Settings />} />
-            <Route path="users" element={<Users />} />
-          </Route>
+              <Route path="cabins" element={<Cabins />} />
+              <Route path="settings" element={<Settings />} />
+              <Route path="users" element={<Users />} />
+            </Route>
 
-          <Route path="login" element={<Login />} />
-          <Route path="*" element={<PageNotFound />} />
-        </Routes>
-      </BrowserRouter>
+            <Route path="login" element={<Login />} />
+            <Route path="*" element={<PageNotFound />} />
+          </Routes>
+        </BrowserRouter>
 
-      <Toaster
-        position="top-center"
-        gutter={12}
-        containerStyle={{ margin: "8px" }}
-        toastOptions={{
-          style: {
-            background: "var(--color-grey-0)",
-            color: "var(--color-grey-700)",
+        <Toaster
+          position="top-center"
+          gutter={12}
+          containerStyle={{ margin: "8px" }}
+          toastOptions={{
+            style: {
+              background: "var(--color-grey-0)",
+              color: "var(--color-grey-700)",
 
-            fontSize: "16px",
-            maxWidth: "500px",
-            padding: "16px 24px",
-          },
+              fontSize: "16px",
+              maxWidth: "500px",
+              padding: "16px 24px",
+            },
 
-          // Default options for specific types
-          success: {
-            duration: 3000,
-          },
+            // Default options for specific types
+            success: {
+              duration: 3000,
+            },
 
-          error: {
-            duration: 5000,
-          },
-        }}
-      />
-    </QueryClientProvider>
+            error: {
+              duration: 5000,
+            },
+          }}
+        />
+      </QueryClientProvider>
+    </DarkModeProvider>
   );
 }
 
---
+++ 17-the-wild-oasis/src/context/DarkModeContext.jsx
@@ -0,0 +1,35 @@
+import { createContext, useContext, useEffect } from "react";
+import { useLocalStorageState } from "../hooks/useLocalStorageState";
+
+const DarkModeContext = createContext();
+
+const DarkModeProvider = ({ children }) => {
+  const [isDarkMode, setIsDarkMode] = useLocalStorageState(false, "dark-mode");
+
+  useEffect(
+    function () {
+      if (isDarkMode) {
+        document.documentElement.classList.add("dark-mode");
+        document.documentElement.classList.remove("light-mode");
+      } else {
+        document.documentElement.classList.add("light-mode");
+        document.documentElement.classList.remove("dark-mode");
+      }
+    },
+    [isDarkMode]
+  );
+
+  function toggleDarkMode() {
+    setIsDarkMode((isDarkMode) => !isDarkMode);
+  }
+
+  return (
+    <DarkModeContext.Provider value={{ isDarkMode, toggleDarkMode }}>
+      {children}
+    </DarkModeContext.Provider>
+  );
+};
+
+const useDarkMode = () => useContext(DarkModeContext);
+
+export { useDarkMode, DarkModeProvider };
