26cf7788b12b187e33ac662fd0bccb3ca91d5425
hassansaber <hassansaberkarin@gmail.com>
2025-01-02 05:20:16 +0330

    set no booking page

--- 17-the-wild-oasis/src/features/bookings/BookingDetail.jsx
+++ 17-the-wild-oasis/src/features/bookings/BookingDetail.jsx
@@ -16,6 +16,7 @@ import { useCheckOut } from "../check-in-out/useCheckOut";
 import { useDeleteBooking } from "./useDeleteBooking";
 import Modal from "../../ui/Modal";
 import ConfirmDelete from "../../ui/ConfirmDelete";
+import Empty from "../../ui/Empty";
 
 const HeadingGroup = styled.div`
   display: flex;
@@ -31,6 +32,8 @@ function BookingDetail() {
   const { deleteBooking, isDeletingBooking } = useDeleteBooking();
 
   if (isLoading) return <Spinner />;
+
+  if (!booking) return <Empty resource="booking" />;
 
   const { status, id: bookingId } = booking;
 
