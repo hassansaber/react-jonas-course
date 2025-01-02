3c7d74a559d63e8fbde4b8fe14b3d7579ad0d8c9
hassansaber <hassansaberkarin@gmail.com>
2025-01-02 05:14:52 +0330

    fix click detection bug for bookings

--- 17-the-wild-oasis/src/ui/Menus.jsx
+++ 17-the-wild-oasis/src/ui/Menus.jsx
@@ -84,10 +84,11 @@ function Menus({ children }) {
 }
 
 function Toggle({ id }) {
-  const { openId, close, open, setPosition } =
-    useContext(MenusContext);
+  const { openId, close, open, setPosition } = useContext(MenusContext);
 
   function handleClick(e) {
+    e.stopPropagation();
+
     const rect = e.target.closest("button").getBoundingClientRect();
     setPosition({
       x: window.innerWidth - rect.width - rect.x,
@@ -106,7 +107,7 @@ function Toggle({ id }) {
 
 function List({ children, id }) {
   const { openId, position, close } = useContext(MenusContext);
-  const ref = useOutsideClick(close);
+  const ref = useOutsideClick(close, false);
 
   if (openId !== id) return null;
 
