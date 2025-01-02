c07b64759cdf890a279017f9de668b9fa15953da
hassansaber <hassansaberkarin@gmail.com>
2025-01-02 05:08:11 +0330

    404 - Error Boundaries

--- 17-the-wild-oasis/src/main.jsx
+++ 17-the-wild-oasis/src/main.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import { StyleSheetManager } from "styled-components";
+import { ErrorBoundary } from "react-error-boundary";
+import ErrorFallback from "./ui/ErrorFallback.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -10,7 +12,12 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       // it looks like an unknown prop is being sent through to the DOM, which will likely trigger a React console error
     }
     <StyleSheetManager shouldForwardProp={() => true}>
-      <App />
+      <ErrorBoundary
+        FallbackComponent={ErrorFallback}
+        onReset={() => window.location.replace("/")}
+      >
+        <App />
+      </ErrorBoundary>
     </StyleSheetManager>
   </React.StrictMode>
 );
