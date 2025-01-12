f4ee4f1176b6cd7d4f1982d9e23aa8461752532a
hassansaber <hassansaberkarin@gmail.com>
2025-01-12 20:49:54 +0330

    422 Navigating Between Pages

--- 21-the-wild-oasis-website/app/page.tsx
+++ 21-the-wild-oasis-website/app/page.tsx
@@ -1,3 +1,10 @@
+import Navigation from "@/components/Navigation";
+
 export default function Page() {
-  return <h1>Wild oasis , welcome to paradise</h1>;
+  return (
+    <div>
+      <Navigation />
+      <h1>Wild oasis , welcome to paradise</h1>;
+    </div>
+  );
 }
