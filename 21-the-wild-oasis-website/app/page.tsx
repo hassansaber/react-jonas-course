f949af1b3c6862da7fa113b7e4dacd23a7c2b8a5
hassansaber <hassansaberkarin@gmail.com>
2025-01-13 00:54:11 +0330

    423 Creating a Layout

--- 21-the-wild-oasis-website/app/page.tsx
+++ 21-the-wild-oasis-website/app/page.tsx
@@ -1,9 +1,6 @@
-import Navigation from "@/components/Navigation";
-
 export default function Page() {
   return (
     <div>
-      <Navigation />
       <h1>Wild oasis , welcome to paradise</h1>;
     </div>
   );
