339ee2a526223a91891fc3f14e6f489711c70389
hassansaber <hassansaberkarin@gmail.com>
2025-01-13 16:00:20 +0330

    425 Fetching Data in a Page

--- 21-the-wild-oasis-website/app/cabins/page.tsx
+++ 21-the-wild-oasis-website/app/cabins/page.tsx
@@ -1,3 +1,39 @@
-export default function page() {
-  return <div>Cabins page</div>;
+interface User {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+}
+
+export default async function page() {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data: User[] = await res.json();
+
+  return (
+    <div>
+      <h1>Cabins page</h1>
+      <ul>
+        {data.map((user) => (
+          <li key={user.id}>{user.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
