3294e44a477a656f4fe8730b20c83463d340cd30
hassansaber <hassansaberkarin@gmail.com>
2025-01-15 02:57:00 +0330

    434 Adding Page Metadata and Favicon

--- 21-the-wild-oasis-website/app/account/page.tsx
+++ 21-the-wild-oasis-website/app/account/page.tsx
@@ -1,3 +1,7 @@
+export const metadata = {
+  title: "Guest ares",
+};
+
 export default function page() {
   return <div>account page</div>;
 }
--- 21-the-wild-oasis-website/app/cabins/page.tsx
+++ 21-the-wild-oasis-website/app/cabins/page.tsx
@@ -1,23 +1,11 @@
-import Counter from "../_components/Counter";
+export const metadata = {
+  title: "Cabins",
+};
 
-export interface User {
-  id: number;
-  name: string;
-}
-
-export default async function page() {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
-  const data: User[] = await res.json();
-
+export default function page() {
   return (
     <div>
       <h1>Cabins page</h1>
-      <ul>
-        {data.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
-      <Counter users={data} />
     </div>
   );
 }
